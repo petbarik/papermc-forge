f908289bcd9d23d4a92037f327a9a7f2fee22f3d
petbarik <150723555+petbarik@users.noreply.github.com>
2026-04-01 13:05:53 +0200

    separeted the css, html and js into separet files

---
+++ script.js
@@ -0,0 +1,22 @@
+function switchTab(tabName, el) {
+document.querySelectorAll('.panel').forEach(p => p.classList.remove('active'));
+document.querySelectorAll('.tab').forEach(t => t.classList.remove('active'));
+
+document.getElementById(tabName).classList.add('active');
+el.classList.add('active');
+}
+
+function openSelectedVersion() {
+const selector = document.getElementById('version');
+const value = selector.value;
+const url = "https://petbarik.github.io/papermc-forge/" + value;
+
+window.open(url, '_blank');
+}
+
+function launchMod(version, modUrl) {
+const url = "https://petbarik.github.io/papermc-forge/" + version +
+"?mod=" + encodeURIComponent(modUrl);
+
+window.open(url, '_blank');
+}
